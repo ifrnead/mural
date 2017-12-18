6a08a1ddb4697deaca36e612d5e45f35765fea0c
Jalerson Lima <jalerson@gmail.com>
2017-12-18 14:05:55 -0300

    Migrando para PostgreSQL.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20171002022250) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "mensagens", force: :cascade do |t|
     t.string   "titulo"
     t.text     "corpo"
